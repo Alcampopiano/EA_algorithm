bug fixes; incrementing only grand average column

EA_algorithm.py
@@ -125,7 +125,7 @@ def init(params_df, map_df):
 
     return df_stu
 
-def adjust_values(df_stu, params_df, map_df, stop_flag=False):
+def get_grand_avgs(df_stu, map_df):
 
     grp_labels = ['group ' + str(g) for g in map_df['group'].unique()]
     group_nums = map_df['group'].unique()
@@ -135,20 +135,80 @@ def adjust_values(df_stu, params_df, map_df, stop_flag=False):
         df_stu[lab]=df_stu[inds].mean(axis=1)
 
     df_stu['mean']=df_stu[grp_labels].agg('mean', axis=1)
-    df_sch = df_stu.groupby(['school'])[['mean']].sum()
-    df_stu.drop('mean', axis=1, inplace=True)
+    #df_sch = df_stu.groupby(['school'])[['mean']].sum()
+    #df_stu.drop('mean', axis=1, inplace=True)
+
+    # # nearest half int
+    # df_sch=df_sch.apply(lambda x: round(x * 2) / 2)
+    # total = df_sch['mean'].sum()
+    # diff = params_df['limit'][0] - total
+    #
+    # #cols = map_df.index
+    #
+    # if (0 <= diff <= params_df['tolerance'][0]) and (total <= params_df['limit'][0]):
+    #
+    #     #df_stu.fillna(0, inplace=True)
+    #
+    #     #df_stu['mean'] = df_stu[grp_labels].agg('mean', axis=1)
+    #
+    #     df_sch['num_of_EAs'] = df_sch['mean']
+    #     df_sch.drop('mean', axis=1, inplace=True)
+    #
+    #     # fill log file
+    #     makeLog(params_df, total, map_df, df_stu, df_sch)
+    #
+    #     stop_flag=True
+    #
+    # elif total<params_df['limit'][0]:
+    #     print('up' + str(total), str(params_df['limit'][0]))
+    #     #df_zeros = df_stu[cols]==0
+    #     # df_stu=df_stu.add((df_stu.select_dtypes(exclude=object) > 0) * .001).combine_first(df_stu) # or even better df[cols] += np.where(df[cols] >0, 0.01, 0)
+    #     #df_stu[cols]
+    #     #df_stu.fillna(0,inplace=True)
+    #     df_stu['mean']+=.001
+    #     df_stu['mean'] = df_stu['mean'].clip_upper(params_df['clip_upper'][0])
+    #     #df_stu[cols] = df_stu[cols].clip_upper(params_df['clip_upper'][0])
+    #
+    # elif total>params_df['limit'][0]:
+    #     print('down' + str(total), str(params_df['limit'][0]))
+    #
+    #     df_stu['mean']-=.001
+    #     df_stu['mean'] = df_stu['mean'].clip_lower(params_df['clip_lower'][0])
+    #
+    #     #df_stu[cols] -= .001
+    #     #df_stu[cols] = df_stu[cols].clip_lower(params_df['clip_lower'][0])
+    #     #df_stu[cols] = df_stu[cols].clip_upper(params_df['clip_upper'][0])
+    #     #df_stu.fillna(0,inplace=True)
+
+    return df_stu
+
+def adjust_values(df_stu, params_df, map_df, stop_flag=False):
+
+    # grp_labels = ['group ' + str(g) for g in map_df['group'].unique()]
+    # group_nums = map_df['group'].unique()
+    #
+    # for lab, num in zip(grp_labels, group_nums):
+    #     inds = map_df[map_df['group'] == num].index
+    #     df_stu[lab]=df_stu[inds].mean(axis=1)
+    #
+    # df_stu['mean']=df_stu[grp_labels].agg('mean', axis=1)
+    # df_sch = df_stu.groupby(['school'])[['mean']].sum()
+    # #df_stu.drop('mean', axis=1, inplace=True)
 
     # nearest half int
+    df_sch = df_stu.groupby(['school'])[['mean']].sum()
     df_sch=df_sch.apply(lambda x: round(x * 2) / 2)
     total = df_sch['mean'].sum()
     diff = params_df['limit'][0] - total
 
-    cols = map_df.index
+    #cols = map_df.index
 
     if (0 <= diff <= params_df['tolerance'][0]) and (total <= params_df['limit'][0]):
 
         #df_stu.fillna(0, inplace=True)
-        df_stu['mean'] = df_stu[grp_labels].agg('mean', axis=1)
+
+        #df_stu['mean'] = df_stu[grp_labels].agg('mean', axis=1)
+
         df_sch['num_of_EAs'] = df_sch['mean']
         df_sch.drop('mean', axis=1, inplace=True)
 
@@ -158,18 +218,25 @@ def adjust_values(df_stu, params_df, map_df, stop_flag=False):
         stop_flag=True
 
     elif total<params_df['limit'][0]:
-        #print('herer')
-        df_stu[cols]=df_stu[cols][df_stu[cols] > 0] + .001
-        df_stu.fillna(0,inplace=True)
-        #df_stu[cols] += .001
-        df_stu[cols] = df_stu[cols].clip_upper(params_df['clip_upper'][0])
+        print('up' + str(total), str(params_df['limit'][0]))
+        #df_zeros = df_stu[cols]==0
+        # df_stu=df_stu.add((df_stu.select_dtypes(exclude=object) > 0) * .001).combine_first(df_stu) # or even better df[cols] += np.where(df[cols] >0, 0.01, 0)
+        #df_stu[cols]
+        #df_stu.fillna(0,inplace=True)
+        df_stu['mean']+=.001
+        df_stu['mean'] = df_stu['mean'].clip_upper(params_df['clip_upper'][0])
+        #df_stu[cols] = df_stu[cols].clip_upper(params_df['clip_upper'][0])
 
     elif total>params_df['limit'][0]:
-        #print('here now')
-        df_stu[cols] -= .001
-        df_stu[cols] = df_stu[cols].clip_lower(params_df['clip_lower'][0])
-        df_stu[cols] = df_stu[cols].clip_upper(params_df['clip_upper'][0])
-        df_stu.fillna(0,inplace=True)
+        print('down' + str(total), str(params_df['limit'][0]))
+
+        df_stu['mean']-=.001
+        df_stu['mean'] = df_stu['mean'].clip_lower(params_df['clip_lower'][0])
+
+        #df_stu[cols] -= .001
+        #df_stu[cols] = df_stu[cols].clip_lower(params_df['clip_lower'][0])
+        #df_stu[cols] = df_stu[cols].clip_upper(params_df['clip_upper'][0])
+        #df_stu.fillna(0,inplace=True)
 
     return df_sch, df_stu, stop_flag
 
@@ -225,6 +292,9 @@ def main():
 
     # read data, set up initial df
     df_stu = init(params_df, map_df)
+
+    # get initital averages
+    df_stu = get_grand_avgs(df_stu, map_df)
 
     while not stop_flag:
         # iter until optimized